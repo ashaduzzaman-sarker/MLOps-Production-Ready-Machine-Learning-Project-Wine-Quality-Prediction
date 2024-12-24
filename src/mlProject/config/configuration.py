859c99de36393e1979ac3fc540d42007f57c40ff
piash <ashaduzzaman2505@gmail.com>
2024-12-25 01:37:27 +0600

    Data Validation Completed

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,7 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import (DataIngestionConfig)
-                                            # DataValidationConfig,
+from mlProject.entity.config_entity import (DataIngestionConfig,
+                                            DataValidationConfig)
                                             # DataTransformationConfig,
                                             # ModelTrainerConfig,
                                             # ModelEvaluationConfig)
@@ -37,20 +37,20 @@ class ConfigurationManager:
     
 
 
-    # def get_data_validation_config(self) -> DataValidationConfig:
-    #     config = self.config.data_validation
-    #     schema = self.schema.COLUMNS
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
 
-    #     create_directories([config.root_dir])
+        create_directories([config.root_dir])
 
-    #     data_validation_config = DataValidationConfig(
-    #         root_dir=config.root_dir,
-    #         STATUS_FILE=config.STATUS_FILE,
-    #         unzip_data_dir = config.unzip_data_dir,
-    #         all_schema=schema,
-    #     )
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
 
-    #     return data_validation_config
+        return data_validation_config
     
 
 
